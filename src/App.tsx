fb5aad63df7e119cdea007a0c1620b04bfda7068
roll1226 <tomo_yasu_4724@icloud.com>
2025-01-11 16:10:53 +0900

    feat: ag grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ColDef } from "ag-grid-community"; // 必要なモジュールをインポート
+import "ag-grid-community/dist/styles/ag-grid.css"; // Ag-Gridの基本スタイル
+import "ag-grid-community/dist/styles/ag-theme-alpine.css"; // テーマスタイル
+import { AgGridReact } from "ag-grid-react";
+
+type Car = {
+  make: string;
+  model: string;
+  price: number;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const columnDefs: ColDef[] = [
+    { field: "make", headerName: "Make" },
+    { field: "model", headerName: "Model" },
+    { field: "price", headerName: "Price" },
+  ];
+
+  const rowData: Car[] = [
+    { make: "Toyota", model: "Celica", price: 35000 },
+    { make: "Ford", model: "Mondeo", price: 32000 },
+    { make: "Porsche", model: "Boxster", price: 72000 },
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        className="ag-theme-alpine"
+        style={{ height: "400px", width: "600px" }}
+      >
+        <AgGridReact
+          rowData={rowData}
+          columnDefs={columnDefs}
+          animateRows={true}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   plugins: [react()],
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "src/styles/variables.scss";`,
+      },
+    },
+  },
+});
