e7d4d2f76a1c809dabd789885b179d9e063dad31
roll1226 <tomo_yasu_4724@icloud.com>
2025-01-13 07:47:29 +0900

    feat: NotFound page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { Checkbox } from "../pages/Checkbox";
 import { Normal } from "../pages/Normal";
+import { NotFound } from "../pages/NotFound";
 
 type CurrentRoutesType = {
   path: string;
@@ -18,5 +19,10 @@ export const CurrentRoutes: CurrentRoutesType[] = [
     path: "/checkbox",
     element: Checkbox,
     title: "Checkbox",
+  },
+  {
+    path: "*",
+    element: NotFound,
+    title: "404 Not Found",
   },
 ];
